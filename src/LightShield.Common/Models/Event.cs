633fdc82d338846eb72d514fe6d80babafd90a4a
sardi10 <sardikuka@gmail.com>
2025-05-26 18:14:56 -0400

    feat(agent): post file events to API (#3)

---
+++ src/LightShield.Common/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿// src/LightShield.Common/Models/Event.cs
+using System;
+
+namespace LightShield.Common.Models
+{
+    public class Event
+    {
+        public string Source { get; set; } = default!;
+        public string Type { get; set; } = default!;
+        public string PathOrMessage { get; set; } = default!;
+        public DateTime Timestamp { get; set; }
+        public string Hostname { get; set; } = default!;
+    }
+}
+
